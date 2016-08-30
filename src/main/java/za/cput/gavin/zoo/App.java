4650e27ba8a2f86251b90eddfa0cf58653697d89
gavin joel ackerman <gavin.j.ackerman@gmail.com>
2016-08-30 13:42:27 +0200

    no message

--- src/main/java/za/cput/gavin/zoo/App.java
+++ src/main/java/za/cput/gavin/zoo/App.java
@@ -18,7 +18,7 @@ public class App
 {
     public static void main( String[] args )
     {
-		System.out.println( "Hello World!" );
-       // SpringApplication.run(App.class, args);
+		//System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
